b486415ca94fd0cf195a9efbb07828fc91851c3d
jaymin <jaymin@promactinfo.com>
2017-10-10 11:38:01 +0530

    First commit

---
+++ index.py
@@ -0,0 +1,10 @@
+import json
+
+print('Loading function')
+def handler(event,context):
+    print("value1 = " + event['key1'])
+    print("value2 = " + event['key2'])
+    print("value3 = " + event['key3'])
+    print('Request Id - '+ context.aws_request_id)
+    return event['key1']  # Echo back the first key value
+
